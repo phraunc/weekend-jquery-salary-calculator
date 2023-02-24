c680a442ae5d59e1ddef3d5ff1ead22ac1df2be2
Anthony Cole <anthony.cole83@yahoo.com>
2023-02-24 16:52:08 -0600

    submit button gets all employee information to the employee array as well as got my render function complete.

--- SCRIPT/salaryCalculator.js
+++ SCRIPT/salaryCalculator.js
@@ -19,17 +19,35 @@ let employee = []; // empty array to capture all employees plus object literal i
 
 function addEmployee(){
     console.log('in the addEmploee function');
-//should capture employee object
+    //should capture employee object
       
-    let employee = {
-        firstname: firstName,
-        lastName: lastName,
-        idNumber: idNumber,
-        jobTitle: jobTitle,
-        annualSalary: annualSalary
-    }
+    const submitFirstName = $('#firstNameInput').val();
+    const submitLastName = $('#lastNameInput').val();
+    const submitIdNumber = $('#employeeIdInput').val();
+    const submitJobTitle = $('#employeeIdInput').val();
+    const submitAnuualSalary = $('#annualSalaryInput').val();
 
+    const NewEmployee = {
+        firstName: submitFirstName,
+        lastName: submitLastName,
+        employeeId: submitIdNumber,
+        jobTitle: submitJobTitle,
+        annualSalary: submitAnuualSalary
+    };
+
+    if(submitFirstName && submitLastName && submitIdNumber && submitJobTitle && submitAnuualSalary) {
+        console.log(true);
+        //if truthy, then run function
+       employee.push(NewEmployee);
+       console.log('Welcome aboard!', NewEmployee);
+}else{
+console.log('Error Bitch!')
+alert('Your submission is missing inputs');
+
+render();
 }
+}
+
 
 //adding a function to delete employee when hit
 
@@ -37,3 +55,32 @@ function removeEmployee(){
 console.log('In the removeEmployee function');
 
 }
+
+function render(){
+console.log('In the render function');
+
+$('#workplace').empty();
+
+for(person of employee ){
+
+    $('#workplace').append(`
+     
+    <ul>
+        <li>${person.firstName}</li>
+        <li>${person.lastName}</li>
+        <li>${person.employeeId}</li>
+        <li>${person.jobTitle}</li>
+        <li>${person.annualSalary}</li>
+        <li>
+            <button class="removeCarBtn">
+            Delete
+            </button>
+        </li>
+    </ul>
+   
+    `)
+
+
+}
+}
+
